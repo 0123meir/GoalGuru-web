0f3ce2802e126d37801b8d96e025f483ae1397dc
Meir <0123meir@gmail.com>
2025-03-22 15:56:25 +0200

    empty page fix

--- client/src/components/EmptyPostsState.tsx
+++ client/src/components/EmptyPostsState.tsx
@@ -3,12 +3,11 @@ import EmptyPostsIcon from "@/assets/EmptyPosts";
 const EmptyPostsState = () => {
   return (
     <div className="flex flex-col items-center justify-center py-16 px-4 text-center">
-      <div className="w-40 h-40 mb-6">
-        <EmptyPostsIcon />
-      </div>
+      <EmptyPostsIcon />
       <h3 className="text-xl font-medium text-gray-800 mb-2">No posts yet</h3>
       <p className="text-gray-600 max-w-md">
-        Looks like there aren't any posts here yet. Why not create one to get started?
+        Looks like there aren't any posts here yet. Why not create one to get
+        started?
       </p>
     </div>
   );
